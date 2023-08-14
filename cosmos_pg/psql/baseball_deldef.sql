fe998448015592aef9c6a2fbe54a0252f436592a
Chris Joakim <christopher.joakim@gmail.com>
2023-08-14 12:43:46 -0400

    Updated cosmos_pg/psql/baseball_deldef.sql - more cols, added batters table

--- cosmos_pg/psql/baseball_deldef.sql
+++ cosmos_pg/psql/baseball_deldef.sql
@@ -15,11 +15,60 @@ create table players (
   throws               CHAR(1),
   category             VARCHAR(16),
   primary_position     VARCHAR(2),
+  primary_team         VARCHAR(8),
+  debut_year           INTEGER,
+  final_year           INTEGER,
+  total_games          INTEGER,
   teams_data           jsonb,
   pitching_data        jsonb,
   batting_data         jsonb,
   embeddings_str       VARCHAR(255),
   embeddings           vector(1536)
+);
+
+create table batters (
+  id                   bigserial primary key,
+  player_id            VARCHAR(32),
+  birth_year           INTEGER,
+  birth_country        VARCHAR(32),
+  first_name           VARCHAR(32),
+  last_name            VARCHAR(32),
+  bats                 CHAR(1),
+  throws               CHAR(1),
+  primary_position     VARCHAR(2),
+  primary_team         VARCHAR(8),
+  debut_year           INTEGER,
+  final_year           INTEGER,
+  total_games          INTEGER,
+
+  atbats               INTEGER,
+  runs                 INTEGER,
+  hits                 INTEGER,
+  doubles              INTEGER,
+  triples              INTEGER,
+  homeruns             INTEGER,
+  rbi                  INTEGER,
+  stolen_bases         INTEGER,
+  caught_stealing      INTEGER,
+  bb                   INTEGER,
+  so                   INTEGER,
+  ibb                  INTEGER,
+  hbp                  INTEGER,
+  sacfly               INTEGER,
+
+  runs_per_ab          NUMERIC(16, 12),
+  batting_avg          NUMERIC(16, 12),
+  double_avg           NUMERIC(16, 12),
+  triple_avg           NUMERIC(16, 12),
+  hr_avg               NUMERIC(16, 12),
+  rbi_avg              NUMERIC(16, 12),
+  bb_avg               NUMERIC(16, 12),
+  so_avg               NUMERIC(16, 12),
+  ibb_avg              NUMERIC(16, 12),
+  hbp_avg              NUMERIC(16, 12),
+  sb_pct               NUMERIC(16, 12)
+
+  embeddings_str       VARCHAR(255)
 );
 
 \d players
